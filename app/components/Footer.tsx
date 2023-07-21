9d76168f88a89545ab5b8dc9e074c8199cc72148
Ben Sehl <ben@sehl.ca>
2023-07-21 12:37:02 -0400

    Simplify fallback footer menu

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -38,43 +38,26 @@ function FooterMenu({menu}: Pick<FooterQuery, 'menu'>) {
 }
 
 const FALLBACK_FOOTER_MENU = {
-  id: 'gid://shopify/Menu/199655620664',
   items: [
     {
-      id: 'gid://shopify/MenuItem/461633060920',
-      resourceId: 'gid://shopify/ShopPolicy/23358046264',
-      tags: [],
+      id: 'gid://shopify/MenuItem/1',
       title: 'Privacy Policy',
-      type: 'SHOP_POLICY',
       url: '/policies/privacy-policy',
-      items: [],
     },
     {
-      id: 'gid://shopify/MenuItem/461633093688',
-      resourceId: 'gid://shopify/ShopPolicy/23358013496',
-      tags: [],
+      id: 'gid://shopify/MenuItem/2',
       title: 'Refund Policy',
-      type: 'SHOP_POLICY',
       url: '/policies/refund-policy',
-      items: [],
     },
     {
-      id: 'gid://shopify/MenuItem/461633126456',
-      resourceId: 'gid://shopify/ShopPolicy/23358111800',
-      tags: [],
+      id: 'gid://shopify/MenuItem/3',
       title: 'Shipping Policy',
-      type: 'SHOP_POLICY',
       url: '/policies/shipping-policy',
-      items: [],
     },
     {
-      id: 'gid://shopify/MenuItem/461633159224',
-      resourceId: 'gid://shopify/ShopPolicy/23358079032',
-      tags: [],
+      id: 'gid://shopify/MenuItem/4',
       title: 'Terms of Service',
-      type: 'SHOP_POLICY',
       url: '/policies/terms-of-service',
-      items: [],
     },
   ],
 };
